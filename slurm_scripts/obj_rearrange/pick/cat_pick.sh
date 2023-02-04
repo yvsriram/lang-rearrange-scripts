36d31fd5eaa2bfcdeb9c4a72ae87c86073fa8084
Sriram Yenamandra <yvsriram99@gmail.com>
2023-02-04 17:31:21 -0500

    updates to pick config

--- slurm_scripts/obj_rearrange/pick/cat_pick.sh
+++ slurm_scripts/obj_rearrange/pick/cat_pick.sh
@@ -4,8 +4,8 @@ export EXP_CONFIG=habitat-baselines/habitat_baselines/config/rearrange/rl_skill.
 export ENVS=16
 export GPUS=8
 
-export INPUTS=obj_category_rgbd
-export OBS_KEYS="['robot_head_depth','robot_head_rgb','object_category','joint','is_holding','relative_resting_position']"
+export INPUTS=obj_embedding_rgbd
+export OBS_KEYS="['robot_head_rgb','object_embedding','joint','is_holding','relative_resting_position']"
 
 export DATA_PATH="data/datasets/floorplanner/v1/\{split\}/cat_split_1_test_scene_10k.json.gz"
 export EPS_KEY="fp"
@@ -16,8 +16,7 @@ export HABITAT_SIM_LOG=quiet
 export WB_RUN_NAME=${EXP_NAME}
 export WB_GROUP=pick
 export MORE_OPTIONS="benchmark/rearrange=cat_pick"
-export MORE_OPTIONS="${MORE_OPTIONS} habitat.task.spawn_max_dists_to_obj=1.5 habitat.task.biased_init=True"
-#export MORE_OPTIONS="${MORE_OPTIONS} habitat_baselines.trainer_name=ddppo"
-#export MORE_OPTIONS="${MORE_OPTIONS} habitat_baselines.rl.ddppo.pretrained_encoder=True habitat_baselines.rl.policy.ovrl=True habitat_baselines.rl.ddppo.pretrained_weights=resnet50_32bp_ovrl.pth habitat_baselines.rl.ddppo.backbone=resnet50"
+export MORE_OPTIONS="${MORE_OPTIONS} habitat.task.spawn_max_dists_to_obj=2.0 habitat.task.biased_init=True"
+export MORE_OPTIONS="${MORE_OPTIONS} habitat_baselines.rl.ddppo.pretrained_encoder=True habitat_baselines.rl.policy.ovrl=True habitat_baselines.rl.ddppo.pretrained_weights=resnet50_32bp_ovrl.pth habitat_baselines.rl.ddppo.backbone=resnet50"
 
 sbatch --gpus ${GPUS} --ntasks-per-node ${GPUS} --error slurm_logs/${EXP_NAME}/err --output slurm_logs/${EXP_NAME}/out lang-rearrange-scripts/slurm_scripts/multi_node_slurm.sh
